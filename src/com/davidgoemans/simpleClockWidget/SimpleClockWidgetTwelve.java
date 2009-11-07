601ee35a74ba68452175d5970e844bd8c845f939
dgoemans <dgoemans@feef39a4-cb9b-11de-a774-41d3a1f294e1>
2009-11-07 12:53:48 +0000

    Added code to repo

---
+++ src/com/davidgoemans/simpleClockWidget/SimpleClockWidgetTwelve.java
@@ -0,0 +1,69 @@
+package com.davidgoemans.simpleClockWidget;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class SimpleClockWidgetTwelve extends AppWidgetProvider 
+{
+    class RunUpdateService extends TimerTask 
+    {
+    	public Context context = null;    	
+    	
+    	public void run() 
+    	{
+   			context.startService(new Intent(context, SimpleClockUpdateServiceTwelve.class));
+    	}
+    }
+    
+    static RunUpdateService m_serviceTask = null;
+    static Timer m_serviceTimer = null;
+
+    
+ 	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) 
+ 	{		
+ 		if( m_serviceTask == null )
+ 		{
+ 			m_serviceTask = new RunUpdateService();
+ 	 		m_serviceTask.context = context;
+ 		}
+ 		
+ 		if( m_serviceTimer == null )
+ 		{
+ 			m_serviceTimer = new Timer();
+ 			m_serviceTimer.schedule(m_serviceTask, 2000, 2000);
+ 		}
+    }
+ 	
+ 	@Override
+ 	public void onDisabled(Context context) {
+ 		// TODO Auto-generated method stub
+ 		super.onDisabled(context);
+ 		
+ 		if ( m_serviceTimer != null )
+ 		{
+ 			m_serviceTimer.cancel();
+ 		}
+ 	}
+ 	
+ 	@Override
+ 	public void onEnabled(Context context) 
+ 	{
+ 		super.onEnabled(context);
+ 		
+ 		if ( m_serviceTimer != null )
+ 		{
+ 			m_serviceTimer.cancel();
+ 		}
+ 		
+ 		m_serviceTask = new RunUpdateService();
+ 		m_serviceTask.context = context;
+ 		m_serviceTimer = new Timer();
+ 		m_serviceTimer.schedule(m_serviceTask, 2000, 2000);
+ 	}
+}
